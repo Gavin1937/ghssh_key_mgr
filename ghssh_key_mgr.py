cc4b5e11c90434c54fe794ca5e7aace6e85e555d
Gavin1937 <gyh1837771475gyh@gmail.com>
2025-03-14 22:56:06 +0800

    allow save password & output to zip

--- ghssh_key_mgr.py
+++ ghssh_key_mgr.py
@@ -9,6 +9,9 @@ from pathlib import Path
 from time import time
 import argparse
 import traceback
+import tempfile
+from shutil import move, rmtree
+from zipfile import ZipFile
 from sshkey_tools.keys import Ed25519PrivateKey
 
 
@@ -108,18 +111,36 @@ def cli_list_gh_keys(config):
   for r in resp:
     print(f'id={r["id"]}, title={r["title"]}')
 
-def cli_create_new_gh_key(config, key_path):
-  key_path = Path(key_path)
-  if not key_path.exists() or not key_path.is_dir():
-    raise ValueError(f'Input key_path doesn\'t exists or isn\'t a directory: {str(key_path)}')
+def cli_create_new_gh_key(config, out_path, save_password=False, save_zip=False):
+  out_path = Path(out_path)
+  if not out_path.exists() or not out_path.is_dir():
+    raise ValueError(f'Input out_path doesn\'t exists or isn\'t a directory: {str(out_path)}')
+  tmp_path = Path(tempfile.mkdtemp())
+  vprint(f'cli_create_new_gh_key(): {tmp_path = }')
   print('Create a new ssh key and upload to your github account...')
-  status,resp = create_ghssh_key(key_path)
+  status,resp = create_ghssh_key(tmp_path)
   if status_not_good(status):
     raise RuntimeError(f'Fail to create a new ssh key and upload to github.')
   print(f'Key Id: {resp["id"]}')
-  print(f'Private Key: {resp["priv_key_path"]}')
-  print(f'Public Key: {resp["pub_key_path"]}')
+  print(f'Private Key: {out_path/Path(resp["priv_key_path"]).name}')
+  print(f'Public Key: {out_path/Path(resp["pub_key_path"]).name}')
   print(f'The password for your key is: {resp["key_password"]}')
+  if save_password:
+    with open(tmp_path/'password.txt', 'w') as file:
+      file.write(resp['key_password'])
+    print(f'The password have been saved to file')
+  if save_zip:
+    with ZipFile(tmp_path/'keys.zip', 'w') as zp:
+      for file in tmp_path.iterdir():
+        if 'keys.zip' not in file.name:
+          zp.write(file, f'keys/{file.name}')
+    for file in tmp_path.iterdir():
+      if 'keys.zip' not in file.name:
+        file.unlink()
+  for file in tmp_path.iterdir():
+    move(file, out_path)
+  if save_zip:
+    rmtree(tmp_path, True)
 
 def cli_remove_gh_key(config, key_id):
   print(f'Remove ssh key [{key_id}] from your github account...')
@@ -159,6 +180,16 @@ def main():
     help='Set output key folder'
   )
   parser.add_argument(
+    '-P','--password',
+    action='store_true', default=False,
+    help='Also save password to file'
+  )
+  parser.add_argument(
+    '-z','--zip',
+    action='store_true', default=False,
+    help='Create a zip archive contains ssh keys & password file. You can use -o to set archive output folder'
+  )
+  parser.add_argument(
     '-v','--verbose',
     action='store_true', default=False,
     help='Toggle verbose mode'
@@ -195,11 +226,13 @@ def main():
       cli_list_gh_keys(config)
     elif args.create:
       if args.output and len(args.output) > 0:
-        key_folder = args.output
+        out_folder = args.output
       else:
-        key_folder = Path('./keys').resolve()
-        key_folder.mkdir(parents=True, exist_ok=True)
-      cli_create_new_gh_key(config, key_folder)
+        out_folder = Path('./keys').resolve()
+        out_folder.mkdir(parents=True, exist_ok=True)
+      save_password = args.password
+      save_zip = args.zip
+      cli_create_new_gh_key(config, out_folder, save_password, save_zip)
     elif args.remove > 0:
       cli_remove_gh_key(config, args.remove)
     else:
